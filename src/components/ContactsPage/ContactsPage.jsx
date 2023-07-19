1f1599284b1df165e8ee39debf44f3e24586c366
Nuno Colaço <nunocolaco@Nunos-MacBook-Pro.local>
2023-07-19 03:15:09 +0100

    Solve a zindex problem on the navbar section and add 4 carousels to the galery page

--- src/components/ContactsPage/ContactsPage.jsx
+++ src/components/ContactsPage/ContactsPage.jsx
@@ -5,12 +5,29 @@ import "./ContactsPage.css";
 import Footer from "../Footer/Footer";
 
 const ContactsPage = () => {
-  const phoneNumber = "912 625 557";
-  const storeNumber = "211 929 411";
+  const phoneNumber = 912625557;
+  const storeNumber = 211929411;
 
   return (
     <>
       <div className="contacts-page">
+        <div>
+          <h1>Delivery</h1>
+          <p>Entregas num raio de 2 kms. Valor mínimo para entrega de 20€.</p>
+        </div>
+
+        <div className="map-section">
+          <h1>Localização</h1>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3113.1768992886728!2d-9.166853984655026!3d38.71374737959947!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd193361abcff4df%3A0x3204be170d95ac18!2sMaritaca!5e0!3m2!1spt-PT!2spt!4v1689645242417!5m2!1spt-PT!2spt"
+            width="100%"
+            height="400"
+            allowFullScreen=""
+            style={{ border: "0" }}
+            loading="lazy"
+            referrerPolicy="origin"
+          ></iframe>
+        </div>
         <div>
           <h1>Contactos</h1>
           <h2>Morada</h2>
@@ -31,7 +48,7 @@ const ContactsPage = () => {
               style={{ textDecoration: "none", color: "inherit" }}
               href={`tel:${phoneNumber}`}
             >
-              {phoneNumber}
+              912 625 557
             </a>
           </p>
           <p>
@@ -39,31 +56,8 @@ const ContactsPage = () => {
               style={{ textDecoration: "none", color: "inherit" }}
               href={`tel:${storeNumber}`}
             >
-              {storeNumber}
+              211 929 411
             </a>
-          </p>
-        </div>
-
-        <div className="map-section">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3113.1768992886728!2d-9.166853984655026!3d38.71374737959947!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd193361abcff4df%3A0x3204be170d95ac18!2sMaritaca!5e0!3m2!1spt-PT!2spt!4v1689645242417!5m2!1spt-PT!2spt"
-            width="100%"
-            height="400"
-            allowfullscreen=""
-            style={{ border: "0" }}
-            loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"
-          ></iframe>
-        </div>
-
-        <div>
-          <h1>Delivery</h1>
-          <p>
-            Entregas três vezes por semana (terças, quintas-feiras e sábados,
-            com uma antecedência mínima de 24 horas), em Lisboa e nos concelhos
-            de Oeiras e Cascais, a partir de um valor mínimo para entrega
-            consoante a área geográfica, e com valores de entrega entre os 5€ e
-            os 20€.
           </p>
         </div>
       </div>
--- src/components/GalleryPage/GalleryPage.jsx
+++ src/components/GalleryPage/GalleryPage.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import "./GalleryPage.css";
 import Footer from "../Footer/Footer";
+import GalleryCard from "./GalleryCard/GalleryCard";
 
 const GalleryPage = () => {
   return (
     <div className="gallery-page">
+      <GalleryCard />
+      {/* <h1>Galeria</h1>
       <h1>Galeria</h1>
       <h1>Galeria</h1>
       <h1>Galeria</h1>
       <h1>Galeria</h1>
-      <h1>Galeria</h1>
-      <h1>Galeria</h1>
+      <h1>Galeria</h1> */}
       <Footer />
     </div>
   );
