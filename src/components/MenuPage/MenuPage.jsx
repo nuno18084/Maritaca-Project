6a7bd224dd56f653cb92bc768d601cc62ca71029
Nuno Colaço <nunocolaco@Nunos-MacBook-Pro.local>
2023-07-27 00:15:19 +0100

    Add the complete menu as well as the ability to open it in a new tab

--- src/components/MenuPage/MenuPage.jsx
+++ src/components/MenuPage/MenuPage.jsx
@@ -8,11 +8,17 @@ import { motion } from "framer-motion";
 const MenuImage = ({ imageURL }) => {
   const imageRef = useRef(null);
 
-  const handleImageClick = () => {
-    if (imageRef.current && imageRef.current.requestFullscreen) {
-      imageRef.current.requestFullscreen().catch((error) => {
-        console.log("Error attempting to enable fullscreen:", error.message);
-      });
+  const handleImageClick1 = () => {
+    if (imageURL === menuImages[0]) {
+      window.open(
+        "https://scontent.flis5-4.fna.fbcdn.net/v/t39.30808-6/345631410_262183662930763_8996491348591735034_n.png?_nc_cat=108&ccb=1-7&_nc_sid=c4c01c&_nc_ohc=ofFHhEq0PhoAX_elz07&_nc_ht=scontent.flis5-4.fna&oh=00_AfBdDrzIHLesuZF-KkOlew74a8IH0Pa3AYGaE6hVg21lWg&oe=64C5F19A",
+        "_blank"
+      );
+    } else {
+      window.open(
+        "https://scontent.flis5-3.fna.fbcdn.net/v/t39.30808-6/345648446_778148770694697_2207015203367209588_n.png?_nc_cat=104&ccb=1-7&_nc_sid=c4c01c&_nc_ohc=bCw5u3XuwBwAX9xOHKS&_nc_ht=scontent.flis5-3.fna&oh=00_AfAjsKxDPZ5SraCEcwnLHsOEPRmhceqt_7xHHdrLbYLUhw&oe=64C5FA28",
+        "_blank"
+      );
     }
   };
 
@@ -22,18 +28,17 @@ const MenuImage = ({ imageURL }) => {
       src={imageURL}
       alt="menu-image"
       className="fullscreen-image"
-      onClick={handleImageClick}
+      onClick={handleImageClick1}
       style={{ width: "25vw", height: "50%" }}
     />
   );
 };
 
+const menuImages = [
+  "https://scontent.flis5-4.fna.fbcdn.net/v/t39.30808-6/345631410_262183662930763_8996491348591735034_n.png?_nc_cat=108&ccb=1-7&_nc_sid=c4c01c&_nc_ohc=ofFHhEq0PhoAX_elz07&_nc_ht=scontent.flis5-4.fna&oh=00_AfBdDrzIHLesuZF-KkOlew74a8IH0Pa3AYGaE6hVg21lWg&oe=64C5F19A",
+  "https://scontent.flis5-3.fna.fbcdn.net/v/t39.30808-6/345648446_778148770694697_2207015203367209588_n.png?_nc_cat=104&ccb=1-7&_nc_sid=c4c01c&_nc_ohc=bCw5u3XuwBwAX9xOHKS&_nc_ht=scontent.flis5-3.fna&oh=00_AfAjsKxDPZ5SraCEcwnLHsOEPRmhceqt_7xHHdrLbYLUhw&oe=64C5FA28",
+];
 const MenuPage = () => {
-  const menuImages = [
-    "https://scontent.flis5-4.fna.fbcdn.net/v/t39.30808-6/345631410_262183662930763_8996491348591735034_n.png?_nc_cat=108&ccb=1-7&_nc_sid=c4c01c&_nc_ohc=ofFHhEq0PhoAX_elz07&_nc_ht=scontent.flis5-4.fna&oh=00_AfBdDrzIHLesuZF-KkOlew74a8IH0Pa3AYGaE6hVg21lWg&oe=64C5F19A",
-    "https://scontent.flis5-3.fna.fbcdn.net/v/t39.30808-6/345648446_778148770694697_2207015203367209588_n.png?_nc_cat=104&ccb=1-7&_nc_sid=c4c01c&_nc_ohc=bCw5u3XuwBwAX9xOHKS&_nc_ht=scontent.flis5-3.fna&oh=00_AfAjsKxDPZ5SraCEcwnLHsOEPRmhceqt_7xHHdrLbYLUhw&oe=64C5FA28",
-  ];
-
   return (
     <>
       <motion.div
