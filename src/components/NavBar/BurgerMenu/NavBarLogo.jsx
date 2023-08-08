03cd6c3e939a83cb3023518ecdd941c8415e0a1b
nuno18084 <nuno.colaco96@gmail.com>
2023-08-08 19:53:47 +0100

    Solve the z-index problem with the logo on the navbar

--- src/components/NavBar/BurgerMenu/NavBarLogo.jsx
+++ src/components/NavBar/BurgerMenu/NavBarLogo.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import BurgerMenu from "./BurgerMenu";
 import BirdLogo from "../../../Images/BirdLogo.png";
+import "./BurgerMenu.css";
 
 const NavBarLogo = () => {
   return (
-    <div>
-      <div>
-        <img src={<BirdLogo />} alt="bird-logo" />
-      </div>
+    <div className="navbar-logo-container">
+      <Link to="/">
+        <div>
+          <img src={BirdLogo} alt="bird-logo" width={75} height={50} />
+        </div>
+      </Link>
       <div>
         <BurgerMenu />
       </div>
