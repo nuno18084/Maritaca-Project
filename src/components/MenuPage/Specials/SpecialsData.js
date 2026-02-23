14b1bc37152ef38674e37fe2ce8d2119411d4908
Nuno Colaço <nunocolaco@Mac.Home>
2026-02-23 19:24:58 +0000

    Change some prices and pictures

--- src/components/MenuPage/Specials/SpecialsData.js
+++ src/components/MenuPage/Specials/SpecialsData.js
@@ -3,48 +3,48 @@ const pizzaData = [
     name: "5 Cogumelos",
     ingredients:
       "Molho de Tomate | Mozzarella 100% | Cogumelos Paris, Shiitake, Shimaji, Portobello, Marron | Parmesão",
-    price: 13.5,
+    price: 15,
     soldOut: false,
   },
   {
     name: "Do Bosque",
     ingredients:
       "Molho de Tomate | Mozzarella 100% | Requeijão | Espargos | Ovo | Salsa | Azeite de Trufa | Cebola Confitada | Amêndoa Torrada",
-    price: 13.5,
+    price: 15,
     soldOut: false,
   },
   {
     name: "Terra e Mar",
     ingredients:
       "Molho de Tomate | Mozzarella 100% | Camarão | Pimento Assado | Molho Pesto | Cebola Confitada",
-    price: 13.5,
+    price: 15,
     soldOut: false,
   },
   {
     name: "Marron",
     ingredients:
       "Mozzarella 100% | Cogumelos Marron | Alho | Salsa | Parmesão | Azeite de Trufa ",
-    price: 13.5,
+    price: 15,
     soldOut: false,
   },
   {
     name: "Tomatina",
     ingredients:
       "Molho de Tomate | Mozzarella 100% | Manjericão | Tomate Cherry | Mozzarella Di Bufala | Parmesão",
-    price: 13.5,
+    price: 15,
     soldOut: false,
   },
   {
     name: "Figo",
     ingredients:
       "Mozzarella 100% | Figo | Gorgonzola | Presunto |  Mel | Oregãos",
-    price: 13.5,
+    price: 15,
     soldOut: false,
   },
   {
     name: "Trufa",
     ingredients: "Gorgonzola | Salsa Trufada | Rúcula | Azeite de Trufa",
-    price: 13.5,
+    price: 15,
     soldOut: false,
   },
 ];
@@ -53,31 +53,31 @@ const rolinhoData = [
   {
     name: "Alho",
     ingredients: "Manteiga de Alho | Mozzarella 100%",
-    price: 4,
+    price: 5,
     soldOut: false,
   },
   {
     name: "Diávola",
     ingredients: "Mozzarella 100% | Pepperoni | Oregãos",
-    price: 5.9,
+    price: 6.5,
     soldOut: false,
   },
   {
     name: "Camarão",
     ingredients: "Mozzarella 100% | Camarão | Catupiri | Oregãos",
-    price: 6.5,
+    price: 7.5,
     soldOut: false,
   },
   {
     name: "Anchovas",
     ingredients: "Mozzarella 100% | Anchovas | Alcaparras",
-    price: 6.5,
+    price: 7.5,
     soldOut: false,
   },
   {
     name: "Trufa",
     ingredients: "Queijo Gorgonzola | Salsa Trufada | Azeite de Trufa",
-    price: 7.5,
+    price: 8,
     soldOut: false,
   },
 ];
@@ -86,21 +86,21 @@ const saladData = [
   {
     name: "Pêra e Parmesão",
     ingredients: "Rúcula | Parmesão | Pêra | Azeite | Creme Balsâmico",
-    price: 7.5,
+    price: 11.5,
     soldOut: false,
   },
   {
     name: "Caprese",
     ingredients: "Tomate | Mozzarella Di Bufala",
-    price: 7.5,
+    price: 9.5,
     soldOut: false,
   },
-  {
-    name: "Rúcula",
-    ingredients: "Rúcula | Tomate Cherry | Azeite",
-    price: 5,
-    soldOut: false,
-  },
+  // {
+  //   name: "Rúcula",
+  //   ingredients: "Rúcula | Tomate Cherry | Azeite",
+  //   price: 5,
+  //   soldOut: false,
+  // },
 ];
 
 const promotionsData = [
